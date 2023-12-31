9f79d7e13fda3c3f587a55c6579a61b29a69097d
Lucas Bonomo <bonomo.lucas@gmail.com>
2023-12-31 16:54:10 -0300

    Gigasecond

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,11 @@
 use time::PrimitiveDateTime as DateTime;
+use time::Duration;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {start}");
+    // unimplemented!("What time is a gigasecond later than {start}");
+
+    // Add gigasecond
+    let end = start + Duration::seconds(1_000_000_000);
+    return end
 }
